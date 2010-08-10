46a306d880a2610fa4aab67d7f833d13305e7da4
Malik <malik.al-hallak@gmx.de>
2010-08-10 17:20:02 +0200

    Strahlen

--- Raytracer/src/ray.cpp
+++ Raytracer/src/ray.cpp
@@ -1,14 +1,24 @@
 
 ray::ray():
   origin_(0,0,0,1),
-  direction_(1,1,1,0)
+  direction_(0,0,1,0)
 
 	{}
 
 ray::ray(point o,vector d):
   origin_(o),
   direction_(d)
+
+{
+	//Normalisieren des Richtungsvektors
+
+	double laenge_dir = sqrt(pow(direction_[0],2)+pow(direction_[1],2)+pow(direction_[2],2));
 	
-	{}
+	if(laenge_dir!=1)
+	{
+		direction_ = direction_/laenge_dir;
+
+	}
+}
 
 
